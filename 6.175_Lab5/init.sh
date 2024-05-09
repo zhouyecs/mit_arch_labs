aa8bb752510e5986d4b299b7f665052db6270fbe
Zhou Ye <zhouye@stu.pku.edu.cn>
2024-05-10 01:32:48 +0800

    finish 6.175 lab5

--- 6.175_Lab5/init.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/cambridgehackers/connectal
-curl http://www.dabeaz.com/ply/ply-3.9.tar.gz | tar -zxf -
-ln -s ply-3.9/ply/ connectal/scripts/
-sed -i 's/python script/python2.7 script/g' connectal/Makefile
